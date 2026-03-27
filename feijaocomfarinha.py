5c79336efcb4bec501ec8e1fac558803b2ae24a4
Pixchui <matheuzinhopintosilva@gmail.com>
2026-03-27 14:47:21 -0300

    FAHHHHHHHHH

---
+++ feijaocomfarinha.py
@@ -0,0 +1,25 @@
+import csv
+
+contador = 0
+dados = []
+reg = []
+
+with open('Pokemon.csv', 'r', encoding='utf-8') as arquivo:
+    leitor = csv.DictReader(arquivo)
+
+    for registro in leitor:
+        contador += 1
+        if contador < 6:
+            reg.append(registro)
+
+    for pin in arquivo:
+        dados.append([pin])
+
+    
+    
+    dataset = 'Pokemon.csv'
+
+    print(f'Dataset: {dataset}')
+    print(f'Quantidade de Registros: {contador}')
+    print(f'Nome das Colunas: {leitor.fieldnames}')
+    print(f'5 Primeiros Registros: {reg}')
